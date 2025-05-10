370658fb52e472c1bfe5193eb38d7ef5667ec249
OgunwandeBukunmi <131627044+OgunwandeBukunmi@users.noreply.github.com>
2025-05-10 18:48:19 +0200

    Update router.js

--- router.js
+++ router.js
@@ -104,7 +104,6 @@ router.post("/login",async (req,res)=>{
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     }).json({ message: 'Logged in' ,id:_id});
-      console.log("token sent")
     }catch(err){
       console.log(err)
       res.status(404).json({error : "Invalid email or Password"})
