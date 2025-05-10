9675efb344ae693c0f5f2dfd2f5f9bc64dcb8d95
OgunwandeBukunmi <131627044+OgunwandeBukunmi@users.noreply.github.com>
2025-05-11 01:30:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: 'https://bankapp-frontend-three.vercel.app/',  
   credentials: true                 
 }));
 app.use(express.json())
